4ba96366fa40bc7afa8238492d8c66be3fd76247
chen1119093063 <130541665+chen1119093063@users.noreply.github.com>
2024-04-12 16:34:46 +0800

    Add files via upload

---
+++ mmdetect/mmdet/models/detectors/cq_detector/cq_retinanet.py
@@ -0,0 +1,51 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmdet.registry import MODELS
+from mmdet.utils import ConfigType, OptConfigType, OptMultiConfig
+from ..single_stage import SingleStageDetector
+from typing import List, Tuple, Union
+from torch import Tensor
+import torchvision
+
+@MODELS.register_module()
+class CQ_RetinaNet(SingleStageDetector):
+    """Implementation of `RetinaNet <https://arxiv.org/abs/1708.02002>`_"""
+
+    def __init__(self,
+                 backbone: ConfigType,
+                 neck: ConfigType,
+                 bbox_head: ConfigType,
+                 pre_encoder: ConfigType,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor: OptConfigType = None,
+                 init_cfg: OptMultiConfig = None) -> None:
+        super().__init__(
+            backbone=backbone,
+            neck=neck,
+            bbox_head=bbox_head,
+            train_cfg=train_cfg,
+            test_cfg=test_cfg,
+            data_preprocessor=data_preprocessor,
+            init_cfg=init_cfg)
+        self.pre_encoder = MODELS.build(pre_encoder)
+        
+        
+    def extract_feat(self, batch_inputs: Tensor) -> Tuple[Tensor]:
+            """Extract features.
+
+            Args:
+                batch_inputs (Tensor): Image tensor with shape (N, C, H ,W).
+
+            Returns:
+                tuple[Tensor]: Multi-level features that may have
+                different resolutions.
+            """
+            x = self.pre_encoder(batch_inputs)
+            torchvision.utils.save_image(batch_inputs,"batch_inputs.jpg")
+            torchvision.utils.save_image(x,"123.jpg")
+            x = self.backbone(x)
+            if self.with_neck:
+                x = self.neck(x)
+            return x
+
+        
